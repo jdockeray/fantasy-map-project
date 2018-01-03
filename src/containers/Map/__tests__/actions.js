544e913156bee8dfd5b6889cac3870c0d48dc66e
james <james@MacBook.local>
2018-01-03 22:25:07 +0000

    v simple action tests

---
+++ src/containers/Map/__tests__/actions.js
@@ -0,0 +1,30 @@
+import configureStore from 'redux-mock-store'
+import { addMapItem } from '../actions'
+import { ADD_MAP_ITEM } from '../../../reducers/map'
+
+const middlewares = []
+const mockStore = configureStore(middlewares)
+
+
+it('addMapItem - should dispatch action', () => {
+  // Initialize mockstore with empty state
+  const initialState = {}
+  const store = mockStore(initialState)
+  const item = {
+    src: 'foo/bar/someEpicImage',
+    x: 1234,
+    y: 123,
+  }
+  // Dispatch the action
+  store.dispatch(addMapItem(item))
+
+  // Test if your store dispatched the expected actions
+  const actions = store.getActions()
+  const expectedPayload = {
+    type: ADD_MAP_ITEM,
+    payload: {
+      item,
+    },
+  }
+  expect(actions).toEqual([expectedPayload])
+})
